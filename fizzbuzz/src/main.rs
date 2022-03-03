4a18e48ce3fb954e6cabdc42990803bfb4f9b34e
mupt <anon@anon.com>
2022-03-02 20:32:18 -0500

    fizzbuzz.

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    for i in 1..101 {
+
+        match (i%3, i%5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("buzz"),
+            (_,_) => println!("{}", i)
+        }
+    }
+}
+//
+//        if el % 3 == 0 && el % 5 == 0 {
+//            println!("Fizz Buzz = {}", el);
+//        } else if el % 5 == 0 {
+//            println!("buzz = {}", el);
+//        } else if el % 3 == 0 {
+//            println!("fizz = {}", el);
+//        }
+//    }
---
+++ functions/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    another_function(5, 6);
+}
+
+
+fn another_function(x: u32, y:u32) {
+    println!("The value of X is {}", x);
+    println!("The value of Y is {}", y);
+}
