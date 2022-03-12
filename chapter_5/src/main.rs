55c762fb717b3ca97aa075153e23743adf2ad1e1
mupt <anon@anon.com>
2022-03-12 14:14:21 -0500

    ch5_structs

---
+++ chapter_5/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+
+    //Structs Page 83
+    //
+
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    fn build_user()
+
+    println!("Hello, world!");
+}
--- sample_book_problems/src/main.rs
+++ sample_book_problems/src/main.rs
@@ -1,15 +1,29 @@
 fn main() {
 
 
+    //First Word String Finder
+    let s = String::from("hellosss eggs");
+    let word = first_word(&s);
+    println!("Your Length Is {}", word);
 
-    let s = String::from("hello");
 
-    first_word(&s);
-    println!("{}", first_word(&s));
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+
+    println!("{}", slice);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
 
-    return s.len();
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+
+    }
+
+    &s[..]
 }
 
